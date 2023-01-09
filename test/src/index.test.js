94d96a87d8fcb7e478597a6e5946b29e5436368e
softwaremakassar <softwaremakassar@gmail.com>
2023-01-09 15:19:49 +0800

    feat(src): add module index

--- test/src/index.test.js
+++ test/src/index.test.js
@@ -1,3 +1,52 @@
-describe("test module index.js", () => {
-  it("init test", () => {});
+const Connector = require('../../src');
+
+describe('test module index.js', () => {
+  it('module should be an object', () => {
+    // Given
+    const expectedDataTypes = 'object';
+
+    // When
+    const moduleDataTypes = typeof Connector;
+
+    // Then
+    expect(expectedDataTypes).toStrictEqual(moduleDataTypes);
+  });
+
+  it('module should contain certain properties with certain data types', () => {
+    // Given
+    const expectedProperties = {
+      createStrapiModule: 'function',
+      deleteStrapiModule: 'function'
+    };
+
+    // When
+    const checkArrayProperties = [];
+    for (const item in expectedProperties) {
+      if (Connector?.[item])
+        if (typeof Connector[item] === expectedProperties[item]) {
+          checkArrayProperties.push(true);
+          continue;
+        }
+      checkArrayProperties.push(false);
+    }
+
+    // Then
+    expect(checkArrayProperties.length).toBeGreaterThan(0);
+    expect(checkArrayProperties.every((x) => x === true)).toStrictEqual(true);
+  });
+
+  it('should have immutable properties', () => {
+    // Given
+    const expectedDataTypes = 'function';
+
+    // When
+    Connector.createStrapiModule = 'test';
+    Connector.hijackNewModule = () => {};
+
+    // Then
+    expect(typeof Connector.createStrapiModule).toStrictEqual(
+      expectedDataTypes
+    );
+    expect(Connector.hijackNewModule).toBe(undefined);
+  });
 });
