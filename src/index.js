58430b6240af50f51b785408357071113cdafa8e
softwaremakassar <softwaremakassar@gmail.com>
2023-01-09 15:19:49 +0800

    feat(src): add module index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-module.exports = {};
+/**
+ * Author: Munir AP
+ * see: https://github.com/lontarscript/sequelize-strapi-connector
+ */
+module.exports = Object.freeze({
+  createStrapiModule: require('./createStrapiModule'),
+  deleteStrapiModule: require('./deleteStrapiModule')
+});
